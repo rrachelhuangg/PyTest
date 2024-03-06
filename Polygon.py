140a88c1b7a26e0c67f0a9eeb43ea85cfcb0f2f8
jtaylor05 <jacksontay666@gmail.com>
2024-03-05 22:09:12 -0500

    shortening timespan and period for aggs

--- Polygon.py
+++ Polygon.py
@@ -1,38 +1,31 @@
 from polygon import RESTClient
 
-<<<<<<< HEAD
-client = RESTClient()
-=======
 client = RESTClient(api_key="h8IoGYiQLCNjfJfU7je02JpmLrk4SJF9")
->>>>>>> 4156c5522612c23c5c773aedb1a51e56644ac641
 
 ticker = "AAPL"
 
 # List Aggregates (Bars)
 aggs = []
-for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_="2023-01-01", to="2023-06-13", limit=50000):
+for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="hour", from_="2024-03-04", to="2024-03-05", limit=50000):
     aggs.append(a)
 
-<<<<<<< HEAD
 print(aggs)
+print(len(aggs))
 
 # Get Last Trade
-trade = client.get_last_trade(ticker=ticker)
-print(trade)
+#trade = client.get_last_trade(ticker=ticker)
+#print(trade)
 
 # List Trades
-trades = client.list_trades(ticker=ticker, timestamp="2022-01-04")
-for trade in trades:
-    print(trade)
+#trades = client.list_trades(ticker=ticker, timestamp="2022-01-04")
+#for trade in trades:
+    #print(trade)
 
 # Get Last Quote
-quote = client.get_last_quote(ticker=ticker)
-print(quote)
+#quote = client.get_last_quote(ticker=ticker)
+#print(quote)
 
 # List Quotes
-quotes = client.list_quotes(ticker=ticker, timestamp="2022-01-04")
-for quote in quotes:
-    print(quote)
-=======
-print(aggs)
->>>>>>> 4156c5522612c23c5c773aedb1a51e56644ac641
+#quotes = client.list_quotes(ticker=ticker, timestamp="2022-01-04")
+#for quote in quotes:
+    #print(quote)
