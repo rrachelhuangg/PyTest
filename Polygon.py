4156c5522612c23c5c773aedb1a51e56644ac641
Rachel Huang <rachelhuang505@gmail.com>
2024-03-05 21:47:58 -0500

    test API call!

--- Polygon.py
+++ Polygon.py
@@ -1,3 +1,12 @@
 from polygon import RESTClient
 
-print("hello world!")
+client = RESTClient(api_key="h8IoGYiQLCNjfJfU7je02JpmLrk4SJF9")
+
+ticker = "AAPL"
+
+# List Aggregates (Bars)
+aggs = []
+for a in client.list_aggs(ticker=ticker, multiplier=1, timespan="minute", from_="2023-01-01", to="2023-06-13", limit=50000):
+    aggs.append(a)
+
+print(aggs)
