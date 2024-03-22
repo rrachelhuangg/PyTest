add8e509c485170129c5d3478f52807effdd5347
Rachel Huang <rachelhuang505@gmail.com>
2024-03-22 10:34:44 -0400

    working on integrating flask server

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from "react";
 
 function NavBar(){
   return(<>
@@ -11,9 +12,32 @@ function NavBar(){
 </ul></>);
 }
 
-function Content(){
-  return(<>
-  <div class="container">
+function App() {
+  const [data, setdata] = useState({
+    name: "",
+    age: 0,
+    date: "",
+    programming: "",
+});
+useEffect(() => {
+  // Using fetch to fetch the api from 
+  // flask server it will be redirected to proxy
+  fetch("/data").then((res) =>
+      res.json().then((data) => {
+          // Setting a data from api
+          setdata({
+              name: data.Name,
+              age: data.Age,
+              date: data.Date,
+              programming: data.programming,
+          });
+      })
+  );
+}, []);
+  return (
+    <>
+    <NavBar/>
+    <div class="container">
 	   <div class="column">
 	     <h2> User's current stats </h2>
 		 <p> money</p>
@@ -25,17 +49,13 @@ function Content(){
      <p>info from our model</p>
 	   </div>
 	   <div class="column">
-	     <h2> other info </h2>
-		 <p> ---</p>
+      <h2>Displaying Data from Python Flask backend</h2>
+     <p>{data.name}</p>
+                <p>{data.age}</p>
+                <p>{data.date}</p>
+                <p>{data.programming}</p>
 	   </div>
-	</div></>);
-}
-
-function App() {
-  return (
-    <>
-    <NavBar/>
-    <Content/>
+	</div>
   </>
   );
 }
